2d3acef769f270172b9d9a79e63000ebd54ee024
chenyangdream <chenyangseamoon@163.com>
2018-12-14 14:38:09 +0800

    添加client类

--- main.py
+++ main.py
@@ -4,3 +4,16 @@ print("hello world")
 
 class Server(object):
     name = 'Server'
+
+class Client(object):
+    name = 'Client'
+    def __init__(self, type):
+        self.type = type
+
+
+class PCClient(Client):
+    def __init__(self, type):
+        super(PCClient, self).__init__(type)
+        self.format = ''
+
+
