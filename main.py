dcee681e52852cfa16881351f19d6ab2997c6a17
chenyangdream <chenyangseamoon@163.com>
2018-12-14 14:29:03 +0800

    添加类TcpServer 添加构造函数和create函数

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 #!/usr/bin/pyenv
 
 print("hello world")
+
+class Server(object):
+    name = 'Server'
+
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+
+    def create(self):
+        print("create a server") 
+
+
+class TcpServer(Server):
+    name = 'TcpServer'
+
+    def __init__(self, ip, port):
+        super(TcpServer, self).__init__(ip, port)
+        self.type = 'TCP'
