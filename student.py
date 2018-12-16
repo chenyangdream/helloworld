ec8dcdfb045897dc6b46441a871e0bb97b3f4646
chenyangdream <chenyangseamoon@163.com>
2018-12-16 19:36:44 +0800

    添加middle school student

--- student.py
+++ student.py
@@ -9,3 +9,11 @@ class Student(object):
     def print():
         print("I am a Student.")
 
+class MiddleSchoolStudent(Student):
+
+    def __init__():
+        self.no = None
+        self.name = ''
+
+    def print():
+        print("I am a Middle School Student.")
