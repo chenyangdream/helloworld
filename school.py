5c4c0cb806bcf7f32a7c006928742395be69c7b4
chenyangdream <chenyangseamoon@163.com>
2018-12-16 15:58:08 +0800

    添加student类和school类 并修改文件为school.py

---
+++ school.py
@@ -0,0 +1,4 @@
+class School(object):
+    def __init__(self, name):
+        self.name = name
+
--- student.py
+++ student.py
@@ -5,3 +5,7 @@ class Student(object):
     def __init__():
         self.no = None
         self.name = ''
+
+    def print():
+        print("I am a student.")
+
