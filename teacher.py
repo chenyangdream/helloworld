a08bdc163a765f9c36e50f72ba611bb4a7c3307a
chenyangdream <chenyangseamoon@163.com>
2018-12-16 20:09:05 +0800

    添加middle school teacher

--- teacher.py
+++ teacher.py
@@ -7,3 +7,11 @@ class Teacher(object):
 
     def print(self):
         print("I am a teacher.Name is %s" % self.name)
+        
+class MiddleSchoolTeacher(object):
+
+    def __init__(self, name):
+        self.name = name
+
+    def print(self):
+        print("I am a Middle School Teacher.Name is %s" % self.name)
