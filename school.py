cb5526b3cd30dc542657ea4591bee5755837edb0
chenyangdream <chenyangseamoon@163.com>
2018-12-16 16:04:14 +0800

    添加teacher类

--- school.py
+++ school.py
@@ -1,4 +1,7 @@
-class School(object):
-    def __init__(self, name):
-        self.name = name
+#!/usr/bin/env python
 
+class School(object):
+
+    def __init__(self, name, region):
+        self.name = name
+        self.region = region
---
+++ teacher.py
@@ -0,0 +1,6 @@
+#!/usr/bin/pyenv
+
+class Teacher(object):
+
+    def __init__(self, name):
+        self.name = name
